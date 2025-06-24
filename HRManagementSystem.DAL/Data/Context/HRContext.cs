3aefebaa20b0e6ccbcc951e449c81b83b8b3170a
MostafaMohsen58 <mostafa.elnahas5884@gmail.com>
2025-06-25 02:39:27 +0300

    Add project setup and its architecture

---
+++ HRManagementSystem.DAL/Data/Context/HRContext.cs
@@ -0,0 +1,23 @@
+﻿using HRManagementSystem.DAL.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManagementSystem.DAL.Data.Context
+{
+    public class HRContext : IdentityDbContext<ApplicationUser>
+    {
+        public HRContext(DbContextOptions<HRContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ HRManagementSystem.DAL/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManagementSystem.DAL.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        //public string Address { get; set; }
+        public string Address { get; set; }
+    }
+}
