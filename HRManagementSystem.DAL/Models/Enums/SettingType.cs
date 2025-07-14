d761e26aca987b3a5f2c566279b983edf59b24ef
MostafaMohsen58 <mostafa.elnahas5884@gmail.com>
2025-07-14 04:12:44 +0300

    add Setting Repository , Service and controller

---
+++ HRManagementSystem.DAL/Models/Enums/SettingType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManagementSystem.DAL.Models.Enums
+{
+    public enum SettingType
+    {
+        Hour= 1,
+        Pound =2
+    }
+}
