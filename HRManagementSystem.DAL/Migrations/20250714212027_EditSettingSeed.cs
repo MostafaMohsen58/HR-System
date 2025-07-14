dcd42f97c397dba2992e3d75e84ee9b8faeb3aad
Reem-A-Hikal <reemhikal19@gmail.com>
2025-07-15 00:24:34 +0300

    Fix Setting Data in DB

---
+++ HRManagementSystem.DAL/Migrations/20250714212027_EditSettingSeed.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRManagementSystem.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditSettingSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Setting",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Deduction", "OverTime" },
+                values: new object[] { 2.00m, 3.00m });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Setting",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Deduction", "OverTime" },
+                values: new object[] { 25.00m, 50.00m });
+        }
+    }
+}
